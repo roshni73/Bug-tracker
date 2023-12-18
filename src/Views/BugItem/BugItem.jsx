270be801ee08c2c67afb012ab16fd6ca46d58288
roshni poudel <99161691+roshni73@users.noreply.github.com>
2023-12-18 21:49:32 +0545

    Priority design updated

--- src/Views/BugItem/BugItem.jsx
+++ src/Views/BugItem/BugItem.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+import '../BugItem/index.css';
 function BugItem({ id, item, modifyItem }) {
     function capitalizeFirstLetter(words) {
         const separateWord = words.toLowerCase().split(' ');
@@ -15,7 +17,11 @@ function BugItem({ id, item, modifyItem }) {
             <td>{capitalizeFirstLetter(item.title)}</td>
             <td>{capitalizeFirstLetter(item.project)}</td>
             <td>{capitalizeFirstLetter(shortDescription)}</td>
-            <td className={`priority-${item.priority.toLowerCase().replace(' ', '-')}`}>{capitalizeFirstLetter(item.priority)}</td>
+            <td>
+                <div className="priority-container">
+                    <span className={`priority-${item.priority.toLowerCase().replace(' ', '-')}`}>{capitalizeFirstLetter(item.priority)}</span>
+                 </div>
+            </td>
             <td>{capitalizeFirstLetter(item.status)}</td>
             <td>
                 <a href="#" onClick={() => modifyItem(item.id, 'edit')} className="edit"><i className="fas fa-edit"></i></a>
